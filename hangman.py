d355cab714f1eae015f7eefd929432bfea7855f7
Jasmine Lawrence <Jasmine@Jasmines-MacBook-Pro-2.local>
2017-10-31 19:35:40 -0700

    always print word progress and print random correct guess praise

--- hangman.py
+++ hangman.py
@@ -28,11 +28,12 @@ def game_setup():
 
 """Lets the user submit a letter guess, validates the input,
 shows the letters they missed or reveals the word with the letters
-they correctly guressed
+they correctly guessed
 
 """
 
 def guess_letter(target_word):
+	correct_guess_text = ["You guessed it!", "Nice job!","That's right!","I see what you did there.", "Keep it up!", "Just a few more to go!"]
 	global chances 
 	global display
 	target = target_word
@@ -58,16 +59,14 @@ def guess_letter(target_word):
 			else:
 				if guess not in discovered:
 					discovered.append(guess)
+
 					#TODO choose a random confirm test string
-					print "That's right!"
+					print correct_guess_text[random.randrange(0, len(correct_guess_text))]
+					#print "That's right!"
 				else:
 					print "You already guessed that. Try again"
 
-				for n in target:
-					if n in discovered:
-						display+= n
-					else:
-						display+='_'
+
 
 		else:
 			if guess == "exit" or guess == "quit":
@@ -76,8 +75,14 @@ def guess_letter(target_word):
 				print "You can only guess one letter at a time"
 	else:
 		print "Try guessing a letter"
-	print display #testing the placement of this
 
+	for n in target:
+		if n in discovered:
+			display+= n
+		else:
+			display+='_'
+	print display #testing the placement of this
+	
 
 	
 #starts the game and shares the victory or defeat message
