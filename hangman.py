25fea800d649ea57c6200374e960e6f40f066e49
Alex Bentley <abentley@onetelcloud.com>
2023-02-06 21:47:13 -0500

    Refactor into OOP. Get rid of globals because it's possible.

--- hangman.py
+++ hangman.py
@@ -1,357 +1,342 @@
-
-''' A revised game of hangman with two game modes. You can guess words at 3
+""" A revised game of hangman with two game modes. You can guess words at 3
 difficulty levels or guess a common phrase. In each mode you only have 6
-chances to guess the right letter. You also have the option to play again. '''
+chances to guess the right letter. You also have the option to play again. """
 
-from ast import IsNot
 import random
 import sys
 import json
 
-#Global Game Vars
-#Update in the 'Reset_values' method if you change these
-chances = 6
-missed = []
-discovered = []
-display = ""
-username = ""
-high_scores = {}
-#these are never reset during a session
-wins = 0
-losses = 0
 
+class Game:
+	def __init__(self):
+		self.chances = 6
+		self.high_scores = {}
+		self.missed = []
+		self.discovered = []
+		self.display = ""
+		self.username = ""
+		self.wins = 0
+		self.losses = 0
 
-'''
-Allows the user to choose between the two game types: words or phrases. 
-'''
-def choose_game_type():
-	options = ['p', 'w', 'P', 'W']
-	choice = input("Choose game type: w for words. p for phrases: ")
-	if choice.isalpha():
-		if len(choice) == 1:
-			if choice in options:
-				choice = choice.lower()
+		self.load_highscore()
+		self.get_username()
+
+	@staticmethod
+	def choose_game_type():
+		"""
+		Allows the user to choose between the two game types: words or phrases.
+
+		:return:
+		"""
+
+		while True:
+			options = ['p', 'w', 'P', 'W']
+			choice = input("Choose game type: w for words. p for phrases: ")
+			if choice.isalpha():
+				if len(choice) == 1:
+					if choice in options:
+						choice = choice.lower()
+						break
+					else:
+						print(bcolors.WARNING + "That's not one of the options I gave you. Try again." + bcolors.ENDC)
+				else:
+					print(bcolors.WARNING + "I only need one letter. Try again." + bcolors.ENDC)
 			else:
-				print(bcolors.WARNING + "That's not one of the options I gave you. You never listen. Goodbye" + bcolors.ENDC)
-				sys.exit()
-		else:
-			print(bcolors.WARNING + "I only need one letter. You never listen. Goodbye" + bcolors.ENDC)
-			sys.exit()
-	else: 
-		print(bcolors.WARNING + "Letters only, gosh. You never listen. Goodbye" + bcolors.ENDC)
-		sys.exit()
+				print(bcolors.WARNING + "Letters only, gosh. Try again." + bcolors.ENDC)
 
-	return choice
+		return choice
 
+	def get_username(self):
+		"""
+		Allows the user to choose a username
 
-def get_username():
-	"""
-	Allows the user to choose a username
-	"""
-	name = input("What's your name? ")
+		:return:
+		"""
+		name = input("What's your name? ")
+		message = f"Welcome back, {name}" if self.high_scores.get(name) else f"Nice to meet you, {name}"
+		print(message)
+		print("I'll be keeping track of your highscore. Good Luck!\n")
+		self.username = name
 
-	if high_scores.get(name) is not None:
-		print("Welcome back, " + name)
-	else:
-		print("Nice to meet you, " + name)
+	@staticmethod
+	def choose_difficulty():
+		"""
+		Allows the user to choose the difficulty of a word game. chooses between
+		easy, medium and hard wordlists.
 
-	print("I'll be keeping track of your highscore. Good Luck! " + '\n')
+		:return:
+		"""
 
-	return name
+		options = ['e', 'E', 'n', 'N', 'h', 'H']
 
-
-def choose_difficulty():
-	""" Allows the user to choose the difficulty of a word game. chooses between
-	easy, medium and hard wordlists."""
-	options = ['e', 'E', 'n', 'N', 'h', 'H']
-
-	choice = input("Choose difficulty: e for easy. n for normal. h for hard. ")
-	if choice.isalpha():
-		if len(choice) == 1:
-			if choice in options:
-				choice = choice.lower()
+		while True:
+			choice = input("Choose difficulty: e for easy. n for normal. h for hard. ")
+			if choice.isalpha():
+				if len(choice) == 1:
+					if choice in options:
+						choice = choice.lower()
+						break
+					else:
+						print(bcolors.WARNING + "That's not one of the options I gave you. Try again." + bcolors.ENDC)
+				else:
+					print(bcolors.WARNING + "I only need one letter. Try again." + bcolors.ENDC)
 			else:
-				print(bcolors.WARNING + "That's not one of the options I game you. You never listen. Goodbye" + bcolors.ENDC)
-				sys.exit()
-		else:
-			print(bcolors.WARNING + "I only need one letter. You never listen. Goodbye" + bcolors.ENDC)
-			sys.exit()
-	else: 
-		print(bcolors.WARNING + "Letters only, gosh. You never listen. Goodbye" + bcolors.ENDC)
-		sys.exit()
+				print(bcolors.WARNING + "Letters only, gosh. Try again." + bcolors.ENDC)
 
-	return choice
+		return choice
 
+	@staticmethod
+	def choose_topic():
+		"""
+		Allows the user to choose the topic of the phrases for a phrase. There are currently
+		options to choose idioms or slogans
 
-def choose_topic():
-	""" Allows the user to choose the topic of the phrases for a phrase. There are currently
-	options to choose idioms or slogans"""
-	options = ['i', 'I', 's', 'S']
+		:return:
+		"""
 
-	choice = input("Choose difficulty: i for idioms. s for slogans: ")
-	if choice.isalpha():
-		if len(choice) ==1:
-			if choice in options:
-				choice = choice.lower()
+		options = ['i', 'I', 's', 'S']
+
+		while True:
+			choice = input("Choose difficulty: i for idioms. s for slogans: ")
+			if choice.isalpha():
+				if len(choice) == 1:
+					if choice in options:
+						choice = choice.lower()
+						break
+					else:
+						print(bcolors.WARNING + "That's not one of the options I gave you. Try again." + bcolors.ENDC)
+				else:
+					print(bcolors.WARNING + "I only need one letter. Try again." + bcolors.ENDC)
 			else:
-				print(bcolors.WARNING + "That's not one of the options I game you. You never listen. Goodbye" + bcolors.ENDC)
-				sys.exit()
-		else:
-			print(bcolors.WARNING + "I only need one letter. You never listen. Goodbye" + bcolors.ENDC)
-			sys.exit()
-	else: 
-		print(bcolors.WARNING + "Letters only, gosh. You never listen. Goodbye" + bcolors.ENDC)
-		sys.exit()
+				print(bcolors.WARNING + "Letters only, please. Try again." + bcolors.ENDC)
 
-	return choice
+		return choice
 
+	@staticmethod
+	def word_game_setup(difficulty):
+		"""
+		Set up game by choosing the target word from a file based on difficulty
 
-def word_game_setup(difficulty):
-	"""
-	Set up game by choosing the target word from a file based on difficulty
-	"""
-	filename_map = {
-		'e': 'easy_hang_words.txt',
-		'n': 'normal_hang_words.txt',
-		'h': 'hang_words.txt'
-	}
+		:param difficulty:
+		:return:
+		"""
 
-	filename = filename_map.get(difficulty, 'easy_hang_words.txt')
+		filename_map = {
+			'e': 'easy_hang_words.txt',
+			'n': 'normal_hang_words.txt',
+			'h': 'hang_words.txt'
+		}
 
-	with open(filename, 'r') as open_file:
-		all_text = open_file.read()
+		filename = filename_map.get(difficulty, 'easy_hang_words.txt')
 
-	word_list = all_text.split("\n")
-	target = random.choice(word_list)
-
-	return target
-
-
-def phrase_game_setup(topic):
-	"""
-	Set up game by choosing the target phrase from a file based on topic
-	"""
-	all_text = ""
-
-	if topic == 'i':
-		with open('idioms.txt', 'r') as open_file:
-			all_text = open_file.read()
-	elif topic == 's':
-		with open('slogans.txt', 'r') as open_file:
+		with open(filename, 'r') as open_file:
 			all_text = open_file.read()
 
-	phrase_list = all_text.split("\n")
-	target = phrase_list[random.randrange(0, len(phrase_list))]
-	target = target.lower()
-	return target
+		word_list = all_text.split("\n")
+		target = random.choice(word_list)
 
+		return target
 
-def guess(targeted, game_type):
-	"""
-	Asks the user to submit a letter guess, then validates the input,
-	shows the letters they missed or reveals the word with the letters
-	they correctly guessed.
+	@staticmethod
+	def phrase_game_setup(topic):
+		"""
+		Set up game by choosing the target phrase from a file based on topic
 
-	"""
-	global chances
-	global display
+		:param topic:
+		:return:
+		"""
 
-	correct_guess_text = [
-		"You guessed it!",
-		"Nice job!",
-		"That's right!",
-		"I see what you did there.",
-		"Keep it up!",
-		"Just a few more to go!"
-	]
-	target = targeted
-	display = ""
-	try_to_solve = False
+		d = {
+			'i': "idioms.txt",
+			's': "slogans.txt"
+		}
 
-	guess = input("Guess a letter or type 'solve': ")
+		with open(d[topic], 'r') as open_file:
+			all_text = open_file.read()
 
-	if guess.isalpha():
-		if len(guess) == 1:
-			guess = guess.lower()
-			if guess in target:
-				if guess not in discovered:
-					discovered.append(guess)
+		phrase_list = all_text.split("\n")
+		target = phrase_list[random.randrange(0, len(phrase_list))]
+		target = target.lower()
+		return target
+
+	def guess(self, targeted, game_type):
+		"""
+		Asks the user to submit a letter guess, then validates the input,
+		shows the letters they missed or reveals the word with the letters
+		they correctly guessed.
+
+		:param targeted:
+		:param game_type:
+		:return:
+		"""
+
+		correct_guess_text = [
+			"You guessed it!",
+			"Nice job!",
+			"That's right!",
+			"I see what you did there.",
+			"Keep it up!",
+			"Just a few more to go!"
+		]
+		target = targeted
+		self.display = ""
+
+		guess = input("Guess a letter or type 'solve': ")
+
+		if guess.isalpha():
+			if len(guess) == 1:
+				guess = guess.lower()
+				if guess in self.discovered:
+					print(bcolors.WARNING + "You already guessed that. Try again." + bcolors.ENDC)
+				elif guess in target:
+					self.discovered.append(guess)
 					print(correct_guess_text[random.randrange(0, len(correct_guess_text))])
 				else:
-					print(bcolors.WARNING + "You already guessed that. Try again." + bcolors.ENDC)
-			else:
-				if guess not in missed:
-					missed.append(guess)
-					print("That's not right. Try again.")
-					chances -= 1
+					if guess not in self.missed:
+						self.missed.append(guess)
+						print("That's not right. Try again.")
+						self.chances -= 1
+					else:
+						print(bcolors.WARNING + "You already guessed that. Try again." + bcolors.ENDC)
+				print("Missed Letters: ", self.missed)
+			elif guess in ["solve", "exit", "quit"]:
+				if guess == "solve":
+					if game_type == 'p':
+						solution = input("Enter the full phrase: ")
+					else:
+						solution = input("Enter the full word: ")
+					result = "win" if solution == target else "lose"
+					self.win_lose(result, game_type, target)
 				else:
-					print(bcolors.WARNING + "You already guessed that. Try again." + bcolors.ENDC)
-			print("Missed Letters: ", missed)
-		elif guess == "solve":
-			try_to_solve = True
-			if game_type == 'p':
-				solution = input("Enter the full phrase: ")
-			else:
-				solution = input("Enter the full word: ")
-		else:
-			if guess == "exit" or guess == "quit":
-				print("Thanks for playing")
-				sys.exit()
+					print("Thanks for playing")
+					sys.exit()
 			else:
 				print(bcolors.WARNING + "You can only guess one letter at a time." + bcolors.ENDC)
-	else:
-		print(bcolors.WARNING + "Try guessing a letter" + bcolors.ENDC)
+		else:
+			print(bcolors.WARNING + "Try guessing a letter" + bcolors.ENDC)
 
-	if try_to_solve:
-		result = you_win if solution == target else you_lose
-		result(game_type, target)
-	else:
-		display = ""
+		self.display = ""
 		for n in target:
-			display += (n + " ") if n in discovered else (' / ' if n == ' ' else '_ ')
-		print(display)
+			self.display += (n + " ") if n in self.discovered else (' / ' if n == ' ' else '_ ')
+		print(self.display)
 
+	def play(self):
+		"""
+		Starts the game, reviews the guessing progress and shares the victory or defeat message
 
-def play():
-	"""Starts the game, reviews the guessing progress and shares the victory or
-	defeat message"""
-	game_type = choose_game_type()
-	if game_type == 'w':
-		difficulty = choose_difficulty()
-		target = word_game_setup(difficulty)
-	elif game_type == 'p':
-		topic = choose_topic()
-		target = phrase_game_setup(topic)
-	challenge = ""
+		:return:
+		"""
 
-	print("Alright, let's get started. Can you solve this? : \n")
-
-	for letter in target:
-		if letter == ' ':
-			challenge += ' / '
-		else:
-			challenge += '_ '
-	print(challenge)
-
-	while chances > 0:
-
-		current_guess = ""
-
-		guess(target, game_type)
-		print("You have %s guesses remaining." % chances)
-		print()
-
-		target_words = display.split("/")
-		for word in target_words:
-			current_guess += word.replace(" ", "") + " "
-		if current_guess[len(current_guess)-1] == ' ':
-			current_guess = current_guess[0:len(current_guess)-1]
-
-		if current_guess == target:
-			you_win(game_type, target)
-
-	else:
-		you_lose(game_type, target)
-		
-
-def you_win(game_type, target):
-	""" Shares the victory messages depending on the game type"""
-	global wins
-
-	print(bcolors.OKGREEN + "You win!" + bcolors.ENDC)
-	if game_type == 'p':
-		print(f"That's right! The correct phrase was: {target}")
-	else:
-		print(f"That's right! The correct word was: {target}")
-
-	wins += 1
-	print(f"You've won {wins} time" + ("s" * (wins != 1)))
-	print()
-	play_again()
-
-
-def you_lose(game_type, target):
-	""" Shares the defeat messages depending on the game type"""
-	global losses
-
-	print(bcolors.FAIL + "Game Over! You Lose" + bcolors.ENDC)
-	if game_type == 'p':
-		print(f"The correct phrase was: {target}")
-	else:
-		print(f"The correct word was: {target}")
-
-	losses += 1
-	print(f"You've lost {losses} time" + ("s" * (losses != 1)))
-	play_again()
-
-
-def play_again():
-	"""
-	Asks the user if they want to play again. Kicks off a new game or ends the game session
-	It also shares the win percentage and times played when leaving the session.
-	"""
-	save_highscore()
-	again = input("Wanna play again? Y/N: ")
-	if again.isalpha():
-		if len(again) == 1:
-			if again.lower() == 'y':
-				print("Yay! Let's go again" + '\n')
-				reset_values()
-				play()
+		wants_to_play = True
+		while wants_to_play:
+			self.chances = 6
+			game_type = self.choose_game_type()
+			if game_type == 'w':
+				difficulty = self.choose_difficulty()
+				target = self.word_game_setup(difficulty)
+			elif game_type == 'p':
+				topic = self.choose_topic()
+				target = self.phrase_game_setup(topic)
 			else:
-				print("Thanks for playing!")
-				rounds = wins+losses
-				percent = (float(wins)/rounds) * 100
-				print("You played %d times" % rounds)
-				print("You won %0.1f%% of games!" % percent)
-				sys.exit()
+				raise Exception("How did this even happen?")
+			challenge = ""
+			for letter in target:
+				challenge += ' / ' if letter == ' ' else '_ '
+			print("Alright, let's get started. Can you solve this? : \n")
+			print(challenge)
+			while self.chances > 0:
+				self.guess(target, game_type)
+				print(f"You have {self.chances} guesses remaining.")
+				print()
+				target_words = self.display.split("/")
+				current_guess = "".join(word.replace(" ", "") for word in target_words)
+				if current_guess == target:
+					self.win_lose("win", game_type, target)
+					wants_to_play = self.play_again()
+					break
+			else:
+				self.win_lose("loss", game_type, target)
+				wants_to_play = self.play_again()
+
+	def win_lose(self, result, game_type, target):
+		"""
+		Print the win/loss result.
+
+		:param result:
+		:param game_type:
+		:param target:
+		:return:
+		"""
+
+		message = ""
+		if result == "win":
+			print(bcolors.OKGREEN + "You win!" + bcolors.ENDC)
+			self.wins += 1
+			message = f"You've won {self.wins} time{'s' if self.wins != 1 else ''}"
+		elif result == "loss":
+			print(bcolors.FAIL + "Game Over! You Lose" + bcolors.ENDC)
+			self.losses += 1
+			message = f"You've lost {self.losses} time{'s' if self.losses != 1 else ''}"
+
+		print(f"The correct {'phrase' if game_type == 'p' else 'word'} was: {target}")
+		print(message)
+
+	def play_again(self):
+		"""
+		Ask whether the user would like to play again
+		:return:
+		"""
+
+		self.save_highscore()
+		again = input("Wanna play again? Y/N: ")
+		if again.lower() == 'y':
+			print("Yay! Let's go again" + '\n')
+			self.reset_values()
+			return True
 		else:
-			print("I'm going to assume you meant no")
-			sys.exit()
-	else:
-		print("I'm going to assume you meant no")
-		sys.exit()
+			print("Thanks for playing!")
+			rounds = self.wins + self.losses
+			percent = (float(self.wins) / rounds) * 100
+			print(f"You played {rounds} times")
+			print(f"You won {percent:.1f}% of games!")
+			return False
 
+	def reset_values(self):
+		"""
+		Resets global game value variables
 
-def reset_values():
-	"""
-	Resets global game value variables
-	"""
-	global chances
-	global missed
-	global discovered
-	global display
-	global username
+		:return:
+		"""
 
-	chances = 6
-	missed = []
-	discovered = []
-	display = ""
-	username = ""
+		self.chances = 6
+		self.missed = []
+		self.discovered = []
+		self.display = ""
 
+	def save_highscore(self):
+		"""
+		Saves the high score of the current user
 
-def save_highscore():
-	"""
-	Saves the high score of the current user
-	"""
-	global username
-	global wins
-	current_score = wins
-	old_score = high_scores.get(username)
+		:return:
+		"""
 
-	if old_score is None or current_score > old_score:
-		high_scores[username] = current_score
-		f = open("highscores.json", "w")
-		json.dump(high_scores, f, indent = 6)
-		f.close()
+		current_score = self.wins
+		old_score = self.high_scores.get(self.username)
 
+		if old_score is None or current_score > old_score:
+			self.high_scores[self.username] = current_score
+			with open("highscores.json", "w") as f:
+				json.dump(self.high_scores, f, indent=6)
 
-def load_highscore():
-	# Opening JSON file
-	f = open('highscores.json',)
-	# returns JSON object as 
-	# a dictionary
-	return json.load(f)
+	def load_highscore(self):
+		"""
+		Load the high scores
+
+		:return:
+		"""
+
+		with open('highscores.json') as f:
+			self.high_scores = json.load(f)
 
 
 class bcolors:
@@ -369,13 +354,12 @@ class bcolors:
 	UNDERLINE = '\033[4m'
 
 
-"""
-The action starts here
-"""
 if __name__ == "__main__":
+	"""
+	The action starts here
+	"""
 	print("Welcome to Hang_words.")
 	print("You can quit the game by typing 'exit' or 'quit' instead of guessing a letter. ")
 	print()
-	high_scores = load_highscore()
-	username = get_username()
-	play()
+	g = Game()
+	g.play()
--- test.py
+++ test.py
@@ -4,47 +4,55 @@ from hangman import *
 
 
 class TestChooseGameType(unittest.TestCase):
+    def __init__(self, methodName):
+        super().__init__(methodName)
+        self.g = Game()
+
     def test_valid_input_w(self):
         print("Type 'w'")
-        self.assertEqual(choose_game_type(), 'w')
+        self.assertEqual(self.g.choose_game_type(), 'w')
 
     def test_valid_input_p(self):
         print("Type 'p'")
-        self.assertEqual(choose_game_type(), 'p')
+        self.assertEqual(self.g.choose_game_type(), 'p')
 
     def test_invalid_input_1(self):
         print("Type '1'")
         with self.assertRaises(SystemExit):
-            choose_game_type()
+            self.g.choose_game_type()
 
     def test_invalid_input_word(self):
         print("Type 'word'")
         with self.assertRaises(SystemExit):
-            choose_game_type()
+            self.g.choose_game_type()
 
     def test_invalid_input_z(self):
         print("Type 'z'")
         with self.assertRaises(SystemExit):
-            choose_game_type()
+            self.g.choose_game_type()
 
 
 class WordGameSetupTest(unittest.TestCase):
+    def __init__(self, methodName):
+        super().__init__(methodName)
+        self.g = Game()
+
     def test_easy_difficulty(self):
         with open('easy_hang_words.txt', 'r') as easy_file:
             easy_words = easy_file.read().split("\n")
-        target = word_game_setup('e')
+        target = self.g.word_game_setup('e')
         self.assertIn(target, easy_words)
 
     def test_normal_difficulty(self):
         with open('normal_hang_words.txt', 'r') as normal_file:
             normal_words = normal_file.read().split("\n")
-        target = word_game_setup('n')
+        target = self.g.word_game_setup('n')
         self.assertIn(target, normal_words)
 
     def test_hard_difficulty(self):
         with open('hang_words.txt', 'r') as hard_file:
             hard_words = hard_file.read().split("\n")
-        target = word_game_setup('h')
+        target = self.g.word_game_setup('h')
         self.assertIn(target, hard_words)
 
 
